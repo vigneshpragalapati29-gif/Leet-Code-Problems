522098e032eb543a28e85148757e3201b74184aa
vigneshpragalapati29-gif <vigneshpragalapati29@gmail.com>
2026-09-23 20:10:35 +0530

    Time: 3 ms (89.7%), Space: 19.4 MB (16.97%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        s=1
+        if num<=1:
+            return False
+        for i in range(2,int(num**0.5)+1):
+            if num%i==0:
+                s+=i
+                s+=num//i
+        if s==num:
+            return True
+        return False
+            
+
+        
